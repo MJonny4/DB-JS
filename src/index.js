6b34db41ce08193134dae2d47afa20e4e230dd6e
MJonny4 <ion2000m@gmail.com>
2023-04-05 20:57:01 +0200

    Finish Slash Commands

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const { Client, IntentsBitField, Events } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
